bcfec73ab71ec57f62aa59f898f64f81494d4710
Sanderson Silva Corrêa <correasanderson@gmail.com>
2021-08-21 15:13:53 -0300

    init

---
+++ SignalIR.Votation/Hubs/PoolHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalIR.Votation.Hubs
+{
+    public class PoolHub : Hub
+    {
+        public async Task SendMessage(string user, string message, string myChannelId, string myChannelVal)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message, myChannelId, myChannelVal).ConfigureAwait(false);
+        }
+    }
+}
